e981351fc78b2a34dc5dea7de21c9352a1285f55
liangzemian <liangzemian@Gmail.com>
2024-12-14 02:21:03 +0800

    webrtc适配

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -483,6 +483,7 @@ extern AVOutputFormat ff_chromaprint_muxer;
 extern AVInputFormat  ff_libgme_demuxer;
 extern AVInputFormat  ff_libmodplug_demuxer;
 extern AVInputFormat  ff_libopenmpt_demuxer;
+extern AVInputFormat  ff_webrtc_demuxer;
 
 #include "libavformat/muxer_list.c"
 #include "libavformat/demuxer_list.c"
